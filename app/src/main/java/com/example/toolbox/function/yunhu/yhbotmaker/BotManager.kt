85ccd63f49f93491d9a0487eb4f7a39064eae3ad
shijuhao <19136769323@163.com>
2026-05-18 20:16:08 +0800

    fix

--- app/src/main/java/com/example/toolbox/function/yunhu/yhbotmaker/BotManager.kt
+++ app/src/main/java/com/example/toolbox/function/yunhu/yhbotmaker/BotManager.kt
@@ -30,6 +30,8 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.background
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
