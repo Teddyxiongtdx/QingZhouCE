adbc3d904bae707b06b6ee6b6fe45a2d6a6264f3
shijuhao <19136769323@163.com>
2026-05-31 11:09:17 +0800

    perf

--- app/src/androidTest/java/com/example/toolbox/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/toolbox/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.juhao.murexide", appContext.packageName)
+        assertEquals("com.example.toolbox", appContext.packageName)
     }
 }
